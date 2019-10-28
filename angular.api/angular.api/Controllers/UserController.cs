d2cb0dfa3306b645cd3b7b96c29980ac2cfc18c7
MatiasAngie <jrodriguez@inifom.gob.ni>
2019-10-28 15:47:06 -0600

    Primer commit

---
+++ angular.api/angular.api/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace angular.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            return Ok(new
+            {
+                Nombre="Yulian",
+                Apellido="Rodiguez",
+                Email="prueba@angular.com"
+            });
+        }
+
+        
+    }
+}
